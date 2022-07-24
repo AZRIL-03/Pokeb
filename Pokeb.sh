de12e4592ae4f39eea2545dae5e1ed3275772e36
AZRIL-03 <109886561+AZRIL-03@users.noreply.github.com>
2022-07-24 11:30:22 +0700

    Add files via upload
    pkg install bash
    pkg install ruby
    pkg install git
    gem install lolcat
    pkg install figlet toilet
    pkg install python python2 python3
    pip install requests bs4 mechanize stdiomask 
    git clone <link git ini>
    Bash or Sh
    bash Pokeb.sh
    
    
                          -Enjoy_for_Vokep-

---
+++ Pokeb.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/bash
+
+s=sleep
+v=https
+z=bit.ly
+k=3ild93L
+
+# Pembersih Layar Utama
+c=clear
+$c
+# Subrek Yt
+echo "Subrek Dulu Kdua Yt ini ... !" | lolcat
+xdg-open https://youtube.com/channel/UCtu-GcxKL8kJBXpR1wfMgWg
+clear
+# Subrek Yt 2
+echo "Subrek Juga Yh Yt ini ....!"
+sleep 2
+xdg-open https://youtube.com/channel/UCnti7B0HaFE0izlHKwZMn8A
+clear
+# Tampilan
+sleep 1
+figlet xnxx | lolcat
+# Author
+echo "◅+++++++++++++++++++++++++++++++++++++++++++▻" | lolcat
+echo "Author: Mr Dark X Speedrun" | lolcat
+echo "Github: https://github.com/Bayu12345677" | lolcat
+echo "Github 2: https://github.com/DARK-02" | lolcat
+echo "Create Script: AzrilGanz :V" | lolcat
+echo "◅+++++++++++++++++++++++++++++++++++++++++++▻" | lolcat
+echo
+# Warna gk ad
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+# daftar menu
+echo "         <•> Welcome Otak Bokep <•>        " | lolcat
+echo "================================================" | lolcat
+echo "1.Video Pokeb" | lolcat
+echo "2.Exit" | lolcat
+echo "================================================" | lolcat
+echo
+# Data Base
+read -p "Pict:  " option;
+if [ $option = "1" ]
+then
+   echo "Sabar Lgi Loading Link...." | lolcat
+   sleep 2
+   echo "↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓" | lolcat
+     echo "l1nk: $e $v://$z/$k" | lolcat
+   echo "↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑" | lolcat
+   echo
+elif [ $option = "2" ]
+then
+    echo "kok kamu keluar sayang💝ku" | lolcat
+else
+  clear
+  sleep 1
+  echo "Anda Salah Input ... !" | lolcat
+  bash Pokeb.sh
+fi
